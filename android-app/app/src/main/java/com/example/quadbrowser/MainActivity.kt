e9dbda4372a22e848dfa20451ba73eb3503f2417
viniciusy30-commits <viniciusy3.0@gmail.com>
2026-09-18 09:51:52 -0300

    Import Toast used by pane feedback

--- android-app/app/src/main/java/com/example/quadbrowser/MainActivity.kt
+++ android-app/app/src/main/java/com/example/quadbrowser/MainActivity.kt
@@ -17,6 +17,7 @@ import android.widget.FrameLayout
 import android.widget.GridLayout
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
