41f8419b7b1d6b869b32a7910d87021a8239e2b3
viniciusy30-commits <viniciusy3.0@gmail.com>
2026-09-21 17:44:50 -0300

    Configure access control endpoint

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,3 +1,8 @@
+val accessControlUrl = providers.gradleProperty("accessControlUrl")
+    .orElse(providers.environmentVariable("ACCESS_CONTROL_URL"))
+    .orElse("")
+    .get()
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -13,6 +18,11 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        buildConfigField("String", "ACCESS_CONTROL_URL", "\"${accessControlUrl.replace("\\", "\\\\").replace("\"", "\\\")}\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
